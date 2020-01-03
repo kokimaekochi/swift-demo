9a36f9738f3d8735d263e545b1320072ffe4919d
kokimaekochi <kokimaekochi@gmail.com>
2020-01-03 19:29:05 +0900

    first commit

---
+++ swift-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-demo
+//
+//  Created by 前幸地光騎 on 2020/01/03.
+//  Copyright © 2020 Koki Maekochi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
